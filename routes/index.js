ed8c2e0869c852adb46b5615ad5862331b049505
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-31 11:10:11 +0530

    Create post router to "/new" path
    This will get user input and push in to messages array
    and redirect "/" path

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,11 @@ router.get("/new", function (req, res, next) {
   res.render("form");
 });
 
+router.post("/new", function (req, res, next) {
+  const { name, message } = req.body;
+  messages.push({ text: message, user: name, added: new Date() });
+
+  res.redirect("/");
+});
+
 module.exports = router;
